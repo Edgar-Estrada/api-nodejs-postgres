1d20e8ec8acd46592e77f0f36b5c9cdbc8a79e67
Edgar Estrada <estradacalvilloedgar@gmail.com>
2021-06-15 12:58:39 -0500

    Added cors

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const {pool} = require('./database');
+const cors = require('cors');
 
 app.set('port',process.env.PORT || 3000);
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors());
 
 app.use('/login',require('./routes/user.routes'));
 
